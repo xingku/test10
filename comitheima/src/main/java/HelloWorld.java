a0bf97d4c8d4af76ae82db9021ad2357f805606f
kx <1351462813@qq.com>
2019-07-26 10:01:18 +0800

    提交项目啦哈哈哈哈哈good

---
+++ comitheima/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("我们打印输出。。。。。。。");
+        System.out.println("我们打印输出。。。。。。。");
+        System.out.println("我们打印输出。。。。。。。");
+        System.out.println("我们打印输出。。。。。。。");
+        System.out.println("我们打印输出。。。。。。。");
+        System.out.println("我们打印输出。。。。。。。");
+    }
+}
