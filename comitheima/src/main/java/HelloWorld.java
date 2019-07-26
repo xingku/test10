909fd60bc7128c1e3c4b42dfce3dad4f574ef082
kx <1351462813@qq.com>
2019-07-26 10:26:00 +0800

    哈哈哈哈哈

--- comitheima/src/main/java/HelloWorld.java
+++ comitheima/src/main/java/HelloWorld.java
@@ -1,10 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("我们打印输出。。。。。。。");
-        System.out.println("我们打印输出。。。。。。。");
-        System.out.println("我们打印输出。。。。。。。");
-        System.out.println("我们打印输出。。。。。。。");
-        System.out.println("我们打印输出。。。。。。。");
-        System.out.println("我们打印输出。。。。。。。");
+        System.out.println("Good,veryGood");
+        System.out.println("Good,veryGood");
+        System.out.println("Good,veryGood");
+        System.out.println("Good,veryGood");
+        System.out.println("Good,veryGood");
+        System.out.println("Good,veryGood");
+        /*修改*/
+
     }
 }
