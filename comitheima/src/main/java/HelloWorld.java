8735c93a8e33344cfe3db59d53da2129ba0986ab
kx <1351462813@qq.com>
2019-07-26 10:46:45 +0800

    我又修改了java代码

--- comitheima/src/main/java/HelloWorld.java
+++ comitheima/src/main/java/HelloWorld.java
@@ -1,11 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Good,veryGood");
-        System.out.println("Good,veryGood");
-        System.out.println("Good,veryGood");
-        System.out.println("Good,veryGood");
-        System.out.println("Good,veryGood");
-        System.out.println("Good,veryGood");
+
         /*修改*/
 
     }
